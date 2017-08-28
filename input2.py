f6fa58dfe1a85032e26cbf94b6d022d617eed4a5
lucascarbone <luc92ac@hotmail.com>
2017-08-28 16:28:00 -0300

    bench variando la cantidad de opiniones

---
+++ input2.py
@@ -0,0 +1,21 @@
+from random import randint, choice
+
+repeticiones = 10
+informantes = 23
+respuestas_min = 5
+respuestas_max = 100
+
+for n in range(0, repeticiones):
+    for respuestas in range(respuestas_min, respuestas_max):
+        caso = []
+        for x in range(0, respuestas):
+            opinion = randint(1, informantes), randint(1, informantes) * choice((-1, 1))
+            while (opinion[0], opinion[1]) in caso or (opinion[0], -opinion[1]) in caso :
+                opinion = randint(1, informantes), randint(1, informantes) * choice((-1, 1))
+            caso.append(opinion)
+
+        print('{} {}'.format(informantes, respuestas))
+        for agenteX, agenteY in caso:
+            print('{} {}'.format(agenteX, agenteY))
+
+print('0 0')

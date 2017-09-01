827191b3b2e9bcfdbdd37e61d3db8457663dbc39
lucascarbone <luc92ac@hotmail.com>
2017-09-01 16:46:52 -0300

    resultados variando opiniones

--- tests.sh
+++ tests.sh
@@ -6,10 +6,10 @@
 # Por ej.: 5 <= informantes <= 100, opiniones = 15
 
 python3 input_variar_informantes.py > input_informantes_5_a_20_opiniones_20
-./main -p0 resultados_p0_informantes_5_a_20_opiniones_15.csv < input_informantes_5_a_20_opiniones_20
-./main -p1a resultados_p1a_informantes_5_a_20_opiniones_15.csv < input_informantes_5_a_20_opiniones_20 
-./main -p1b resultados_p1b_informantes_5_a_20_opiniones_15.csv < input_informantes_5_a_20_opiniones_20 
-./main -p2 resultados_p2_informantes_5_a_20_opiniones_15.csv < input_informantes_5_a_20_opiniones_20
+./main -p0 resultados_p0_informantes_5_a_20_opiniones_20.csv < input_informantes_5_a_20_opiniones_20
+./main -p1a resultados_p1a_informantes_5_a_20_opiniones_20.csv < input_informantes_5_a_20_opiniones_20 
+./main -p1b resultados_p1b_informantes_5_a_20_opiniones_20.csv < input_informantes_5_a_20_opiniones_20 
+./main -p2 resultados_p2_informantes_5_a_20_opiniones_20.csv < input_informantes_5_a_20_opiniones_20
 
 # Gafico 2 (Variar podas y opiniones)
 # Variando el numero de podas (cuatro lineas en el grafico), 
@@ -17,4 +17,7 @@ python3 input_variar_informantes.py > input_informantes_5_a_20_opiniones_20
 # Por ej.: informantes = 20, 5 <= opiniones <= 100
 
 python3 input_variar_opiniones.py > input_informantes_20_opiniones_0_a_100
-./main -p2 resultados_informantes_20_opiniones_2_a_100.csv < input_informantes_20_opiniones_0_a_100 
+./main -p0 resultados_p0_informantes_20_opiniones_0_a_100.csv < input_informantes_20_opiniones_0_a_100 
+./main -p1a resultados_p1a_informantes_20_opiniones_0_a_100.csv < input_informantes_20_opiniones_0_a_100 
+./main -p1b resultados_p1b_informantes_20_opiniones_0_a_100.csv < input_informantes_20_opiniones_0_a_100 
+./main -p2 resultados_p2_informantes_20_opiniones_0_a_100.csv < input_informantes_20_opiniones_0_a_100 
